95ce95e88a9ab5f1fa9d790980e01b93c1349e31
karamakcsc <karamakcsc@hotmail.com>
2023-06-06 21:02:30 +0000

    api

--- master_painter/api.py
+++ master_painter/api.py
@@ -6,22 +6,40 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 import requests , json
 
 
+# @frappe.whitelist()
+# def get_painter_no(mobile_number=None, first_name=None):
+#     return frappe.db.sql("""SELECT tp.mobile_number,tp.first_name
+#                             FROM `tabPainter` tp
+#                             WHERE tp.mobile_number = %s
+#                             ORDER BY tp.creation DESC;""",
+#                             (mobile_number,first_name),as_dict=True)
+
+
+
+
+
 @frappe.whitelist()
 def get_painter_no(mobile_number=None, first_name=None):
-    # return frappe.db.sql("""SELECT tp.mobile_number,tp.first_name
-    #                         FROM `tabPainter` tp
-    #                         WHERE tp.mobile_number = %s
-    #                         ORDER BY tp.creation DESC;""",
-    #                         (mobile_number,first_name),as_dict=True)
+    # Assuming you have imported and configured the necessary modules for Frappe
+ results = frappe.db.sql("""SELECT tp.mobile_number, tp.first_name
+                              FROM `tabPainter` tp
+                              WHERE tp.mobile_number = %s
+                              ORDER BY tp.creation DESC;""",
+                            (mobile_number, first_name),
+                            as_dict=True)
+    
+    return results
 
-
-url = "http://ugc.kcsc.com.jo/api/method/master_painter.api.get_painter_no?mobile_number=null"
-
-headers = {
-  'Authorization': 'Basic NzJkNGZhMjUzNmZhMWIxOjgwYjY1NWYzNmY2MWFjYw==',
-  'Cookie': 'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
+url = "http://ugc.kcsc.com.jo/api/method/master_painter.api.get_painter_no"
+query_params = {
+    'mobile_number': 'null'  # Adjust the value as per your requirements
 }
 
-response = requests.get(url, headers=headers)
+headers = {
+    'Authorization': 'Basic NzJkNGZhMjUzNmZhMWIxOjgwYjY1NWYzNmY2MWFjYw==',
+    'Cookie': 'full_name=Guest; sid=Guest; system_user=no; user_id=Guest; user_image='
+}
+
+response = requests.get(url, headers=headers, params=query_params)
 
 print(response.text)
