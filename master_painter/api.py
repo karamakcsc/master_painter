7433af7eb3f7689d57ac69408ad855d6fac1048b
karamakcsc <karamakcsc@hotmail.com>
2023-06-06 20:28:42 +0000

    api

--- master_painter/api.py
+++ master_painter/api.py
@@ -10,8 +10,7 @@ import requests , json
 def get_painter_no(mobail_number=None):
     return frappe.db.sql("""
         SELECT tp.mobile_number, tp.first_name
-        FROM `tabPainter ` tp
+        FROM `tabPainter` tp
         WHERE tp.mobile_number = %s
         ORDER BY tp.creation DESC;""",
         (mobile_number),as_dict=True)
-        
