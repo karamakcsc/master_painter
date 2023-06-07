5ea3028d50cfbde0db8072e4bcc16e335c8657ea
karamakcsc <karamakcsc@hotmail.com>
2023-06-07 13:48:32 +0000

    api4

--- master_painter/api.py
+++ master_painter/api.py
@@ -6,26 +6,14 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 import requests , json
 
 
-# @frappe.whitelist()
-# def get_painter_no(mobile_number=None, first_name=None):
-#     return frappe.db.sql("""
-#         SELECT tp.mobile_number
-#         FROM `tabPainter` tp
-#         WHERE tp.mobile_number = %s
-#         ORDER BY tp.creation DESC;
-#         """, (mobile_number,), as_dict=True)
-
-
-
 @frappe.whitelist()
 def get_painter_no(mobile_number=None, first_name=None):
     return frappe.db.sql("""
-        SELECT tp.mobile_number, tp.first_name
+        SELECT tp.mobile_number
         FROM `tabPainter` tp
         WHERE tp.mobile_number = %s
         ORDER BY tp.creation DESC;
-        """, (mobile_number, first_name), as_dict=True)
-
+        """, (mobile_number,), as_dict=True)
 
 
 #######################
