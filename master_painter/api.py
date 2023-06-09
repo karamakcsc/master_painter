b5fe1bc38c756fcb4ac66f4392db72aae1f3a8f6
karamakcsc <karamakcsc@hotmail.com>
2023-06-09 20:49:10 +0000

    only phone number-txt

--- master_painter/api.py
+++ master_painter/api.py
@@ -28,10 +28,10 @@ def get_painter_no(mobile_number=None, docstatus=None):
 
     if not result:
         return {
-            'mobile_number': "",
-            'first_name': "",
-            'painter_level': "",
-            'docstatus': ""
+            'mobile_number': ,
+            'first_name': ,
+            'painter_level': ,
+            'docstatus': 
         }
 
     return result
