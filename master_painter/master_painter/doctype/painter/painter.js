b4ac8e9a0e142afb57c9a134c576cab53d1969e8
karamakcsc <karamakcsc@hotmail.com>
2023-06-14 00:14:11 +0000

    Upload File

--- master_painter/master_painter/doctype/painter/painter.js
+++ master_painter/master_painter/doctype/painter/painter.js
@@ -33,24 +33,24 @@ frappe.ui.form.on('Painter', {
 
 
 
-frappe.ui.form.on('Painter', {
-	after_insert: function(frm) {
-	  frappe.msgprint("hi");
-	  frappe.call({
-		method: "master_painter.api.send_sms",
-		args: {
-		  "numbers": frm.doc.name,
-		  "accpass": "pP$psJpsnyYClk2v"
-		},
-		callback: function(response) {
-		  // Handle the response from the server-side method
-		  var result = response.message;
-		  console.log(result);
-		  // Perform any additional actions based on the response
-		}
-	  });
-	}
-  });
+// frappe.ui.form.on('Painter', {
+// 	after_insert: function(frm) {
+// 	  frappe.msgprint("hi");
+// 	  frappe.call({
+// 		method: "master_painter.api.send_sms",
+// 		args: {
+// 		  "numbers": frm.doc.name,
+// 		  "accpass": "pP$psJpsnyYClk2v"
+// 		},
+// 		callback: function(response) {
+// 		  // Handle the response from the server-side method
+// 		  var result = response.message;
+// 		  console.log(result);
+// 		  // Perform any additional actions based on the response
+// 		}
+// 	  });
+// 	}
+//   });
   
 
 
