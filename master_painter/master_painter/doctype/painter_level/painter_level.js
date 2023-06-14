1c2e7536baa99a61dec65d27c924a83feb98b3a3
karamakcsc <karamakcsc@hotmail.com>
2023-06-14 11:21:32 +0000

    Painter Item

---
+++ master_painter/master_painter/doctype/painter_level/painter_level.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, KCSC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Painter Level', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ master_painter/master_painter/doctype/painter_level/test_painter_level.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, KCSC and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPainterLevel(unittest.TestCase):
+	pass
