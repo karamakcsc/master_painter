1d688ae507ec19c45d26a01d5d573e5c5419c704
karamakcsc <karamakcsc@hotmail.com>
2023-06-14 12:22:19 +0000

    Painter Item

--- master_painter/master_painter/doctype/master_painter_item/master_painter_item.js
+++ master_painter/master_painter/doctype/master_painter_item/master_painter_item.js
@@ -6,3 +6,28 @@ frappe.ui.form.on('Master Painter Item', {
 
 	// }
 });
+
+
+
+// cur_frm.fields_dict['point_amount'].get_query = function(doc) {
+// 	return {
+// 		filters: {
+// 			"name": doc.item_category
+// 		}
+// 	}
+// }
+
+
+frappe.ui.form.on('Master Painter Item', {
+	item_category: function(frm) {
+		frappe.call({
+			method: "master_painter.master_painter.doctype.master_painter_item.master_painter_item.get_point_amount",
+			args: {
+				name: frm.doc.item_category // Pass the item category name as an argument
+			},
+			callback: function(r) {
+				frm.set_value('point_amount', r.message); // Set the returned value as the 'point_amount' field value
+			}
+		});
+	}
+});
--- master_painter/master_painter/doctype/master_painter_item/master_painter_item.py
+++ master_painter/master_painter/doctype/master_painter_item/master_painter_item.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, KCSC and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class MasterPainterItem(Document):
 	pass
+
+
+
+@frappe.whitelist()
+def get_point_amount(name=None):
+    result = frappe.db.sql("""
+        SELECT point_amount 
+        FROM `tabMaster Painter Item Category` 
+        WHERE name = %s 
+        ORDER BY creation DESC
+        LIMIT 1
+    """, (name,), as_dict=True)
+    
+    if result:
+        return result[0].point_amount
+    else:
+        return 0  # or any default value if no matching record is found
